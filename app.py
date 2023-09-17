b5fd6eff605c353f826a9b03ec26a435657265c2
Carvalho <edd45@miami.edu>
2023-09-17 10:55:32 -0400

    final

--- app.py
+++ app.py
@@ -2,9 +2,166 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from cs50 import SQL
 from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+import csv
+import datetime
+import pytz
+import requests
+import subprocess
+import urllib
+import uuid
+import os
+
 
 app = Flask(__name__)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Establish database
+db = SQL("sqlite:///users.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Decorated function to have a log in and out (complicated flask stuff)
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+# Render an apology if anything at all goes wrong using github memegen API/repo
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+
 @app.route("/")
+@login_required
 def index():
-    '''opening page'''
+    """opening page"""
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(
+            rows[0]["hash"], request.form.get("password")
+        ):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via "GET" (just trying to access the page), send them to "/register" webpage
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # POST method
+    else:
+        # Check username and password are not blank or username already exists or username is already taken
+        if not request.form.get("username") or not request.form.get("password"):
+            return apology("must provide a username and password")
+
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("password and confirmation don't match")
+
+        rows = db.execute(
+            "SELECT * FROM users WHERE username = ?", request.form.get("username")
+        )
+        if len(rows) != 0:
+            return apology("username is already taken")
+
+        # Generate password hash and insert user into the database
+        passwordHash = generate_password_hash(request.form.get("password"))
+        newUserID = db.execute(
+            "INSERT INTO users (username, hash) VALUES(?, ?)",
+            request.form.get("username"),
+            passwordHash,
+        )
+
+        # log the user in and redirect to homepage
+        session["user_id"] = newUserID
+        return redirect("/")
